608f531879a115b44692d722dfb00e6188b3e6e0
Administrator <Administrator@PC269>
2017-12-22 09:43:40 +0700

    thuchanhlab3

---
+++ TimBoiVaUocChung/Forms/TimBoiVaUoc.Designer.cs
@@ -0,0 +1,228 @@
+﻿using System;
+
+namespace TimBoiVaUocChung.Forms
+{
+    partial class TimBoiVaUoc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtNumB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rdBCNN = new System.Windows.Forms.RadioButton();
+            this.rdUCLN = new System.Windows.Forms.RadioButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtNumB);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtNumA);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(249, 72);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nhập số";
+            // 
+            // txtNumB
+            // 
+            this.txtNumB.Location = new System.Drawing.Point(48, 43);
+            this.txtNumB.Name = "txtNumB";
+            this.txtNumB.Size = new System.Drawing.Size(194, 20);
+            this.txtNumB.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Số B";
+            // 
+            // txtNumA
+            // 
+            this.txtNumA.Location = new System.Drawing.Point(48, 17);
+            this.txtNumA.Name = "txtNumA";
+            this.txtNumA.Size = new System.Drawing.Size(194, 20);
+            this.txtNumA.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Số A";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rdBCNN);
+            this.groupBox2.Controls.Add(this.rdUCLN);
+            this.groupBox2.Location = new System.Drawing.Point(268, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(212, 73);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Trọn phép tính";
+            // 
+            // rdBCNN
+            // 
+            this.rdBCNN.AutoSize = true;
+            this.rdBCNN.Location = new System.Drawing.Point(18, 43);
+            this.rdBCNN.Name = "rdBCNN";
+            this.rdBCNN.Size = new System.Drawing.Size(75, 17);
+            this.rdBCNN.TabIndex = 0;
+            this.rdBCNN.TabStop = true;
+            this.rdBCNN.Text = "Tìm BCNN";
+            this.rdBCNN.UseVisualStyleBackColor = true;
+            // 
+            // rdUCLN
+            // 
+            this.rdUCLN.AutoSize = true;
+            this.rdUCLN.Location = new System.Drawing.Point(18, 21);
+            this.rdUCLN.Name = "rdUCLN";
+            this.rdUCLN.Size = new System.Drawing.Size(74, 17);
+            this.rdUCLN.TabIndex = 0;
+            this.rdUCLN.TabStop = true;
+            this.rdUCLN.Text = "Tìm UCLN";
+            this.rdUCLN.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.groupBox3.Controls.Add(this.btnClose);
+            this.groupBox3.Controls.Add(this.btnDelete);
+            this.groupBox3.Controls.Add(this.btnResult);
+            this.groupBox3.Controls.Add(this.txtResult);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(12, 91);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(468, 105);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Tính kết quả";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(359, 71);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Thoát";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(200, 71);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Xóa";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnResult
+            // 
+            this.btnResult.Location = new System.Drawing.Point(37, 71);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(77, 23);
+            this.btnResult.TabIndex = 2;
+            this.btnResult.Text = "Tính kết quả";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Click += new System.EventHandler(this.btnResult_Click_1);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(90, 24);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(344, 20);
+            this.txtResult.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Kết quả";
+            // 
+            // TimBoiVaUoc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 209);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "TimBoiVaUoc";
+            this.Text = "TimBoiVaUoc";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtNumB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rdBCNN;
+        private System.Windows.Forms.RadioButton rdUCLN;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnResult;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label3;
+ 
+    }
+}
---
+++ TimBoiVaUocChung/Forms/TimBoiVaUoc.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimBoiVaUocChung.Forms
+{
+    public partial class TimBoiVaUoc : Form
+    {
+        public TimBoiVaUoc()
+        {
+            InitializeComponent();
+        }
+        private int numA;
+        private int numB;
+        string errormessage;
+        private void btnResult_Click_1(object sender, EventArgs e)
+        {
+            string strA = txtNumA.Text;
+            string strB = txtNumB.Text;
+            //Kiem tra A va B co hop le khong
+            if (IsValidNumber(strA, strB))
+            {
+                if(!rdUCLN.Checked && !rdBCNN.Checked)
+                {
+                    MessageBox.Show("Vui long chon phep tinh");
+                    return;
+                }
+                //tinh theo phep tinh da chon
+                int result;
+                if (rdUCLN.Checked)
+                    result = TimUCLN();
+                else
+                    result = TimBCNN();
+                //hien ket qua len textbox
+                txtResult.Text = result.ToString();
+            }
+            else
+                MessageBox.Show(errormessage);
+        }
+
+        private int TimBCNN()
+        {
+            return (numA * numB) / TimUCLN();
+        }
+
+        private int TimUCLN()
+        {
+            while(numA != numB)
+            {
+                if (numA > numB)
+                    numA -= numB;
+                else
+                    numB -= numA;
+            }
+            return numA;
+        }
+
+        /// <summary>
+        /// Ham kiem tra A va B co hop le khong
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        private bool IsValidNumber(string a, string b)
+        {
+            errormessage = "";
+            if(string.IsNullOrEmpty(a))
+            {
+                errormessage += "Vui long nhap so A\n";
+            }
+            if (string.IsNullOrEmpty(b))
+            {
+                errormessage += "Vui long nhap so B\n";
+            }
+            try
+            {
+                numA = int.Parse(a);
+                numB = int.Parse(b);
+            }
+            catch (Exception)
+            {
+                errormessage += "A hoac B khong phai la so\n";
+            }
+            //neu A va B la so tra ve true, nguoc lai la false
+            return (errormessage == "") ? true : false;
+        }
+    }
+}
